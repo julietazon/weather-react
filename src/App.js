9fc83efc655a15a8dec8ec23540b21fe4b02a865
julietazon <juubozon@gmail.com>
2021-03-22 16:58:58 -0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import WeatherApp from "./WeatherApp";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="App-wrapper">
+        <WeatherApp />
+      </div>
+    </div>
+  );
 }
 
 export default App;
