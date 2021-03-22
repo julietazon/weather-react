c1d6037c63f085aa1bff588a771de9debc129dd4
julietazon <juubozon@gmail.com>
2021-03-22 17:39:09 -0300

    add temperature link C|F

---
+++ src/WeatherDegrees.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function WeatherDegrees(props) {
+  const [unit, setUnit] = useState("celsius");
+  function showFahren(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  let fahren = (props.celsius * 9) / 5 + 32;
+
+  if (unit === "celsius") {
+    return (
+      <div className="container WeatherDegrees">
+        <span className="MainTemperatureDegrees">
+          {Math.round(props.celsius)}
+        </span>
+        <span className="MainTemperatureUnits">
+          <span className="unit">°C</span> |{" "}
+          <span href="/" onClick={showFahren}>
+            °F
+          </span>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container WeatherDegrees">
+        <span className="MainTemperatureDegrees">{Math.round(fahren)}</span>
+        <span className="MainTemperatureUnits">
+          <span href="/" onClick={showCelsius}>
+            °C
+          </span>{" "}
+          | °F
+        </span>
+      </div>
+    );
+  }
+}
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,5 +1,18 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import WeatherDegrees from "./WeatherDegrees";
 
 export default function WeatherTemperature(props) {
-  return <div className="WeatherTemperature"></div>;
+  return (
+    <div className="WeatherTemperature">
+      <ul>
+        <li>
+          <WeatherIcon code={props.data.icon} alt={props.data.description} />
+        </li>
+        <li>
+          <WeatherDegrees celsius={props.data.temperature} />
+        </li>
+      </ul>
+    </div>
+  );
 }
