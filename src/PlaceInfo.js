5d8b01dddd46e4bc0f7fa548c5a93326ec89e275
julietazon <juubozon@gmail.com>
2021-03-22 17:19:45 -0300

    add PlaceInfo content

--- src/PlaceInfo.js
+++ src/PlaceInfo.js
@@ -1,5 +1,22 @@
 import React from "react";
+import AwesomeDate from "./AwesomeDate";
 
-export default function PlaceInfo() {
-  return <div className="PlaceInfo"></div>;
+export default function PlaceInfo(props) {
+  return (
+    <div className="PlaceInfo">
+      <ul>
+        <li className="PlaceInfo-city">
+          <span>{props.data.city}</span>
+          <span>, </span>
+          <span>{props.data.country}</span>
+        </li>
+        <li>
+          <span className="WeatherDescription">
+            <AwesomeDate date={props.data.date} />
+          </span>
+        </li>
+        <li className="WeatherDescription">{props.data.description}</li>
+      </ul>
+    </div>
+  );
 }
--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -3,44 +3,99 @@ import PlaceInfo from "./PlaceInfo";
 import WeatherTemperature from "./WeatherTemperature";
 import WeatherProperties from "./WeatherProperties";
 import Forecast from "./Forecast";
-import "./WeatherApp.css";
 import axios from "axios";
 import Loader from "react-loader-spinner";
+import "./WeatherApp.css";
 
-export default function WeatherApp() {
-  return (
-    <div className="WeatherApp">
-      <header className="WeatherApp-search">
-        <div className="d-flex justify-content-center Dictionary">
-          <form className="d-flex form">
-            <input
-              type="search"
-              className="form-control"
-              autoComplete="off"
-              autoFocus="on"
-            />
-            <button
-              type="submit"
-              value="Search"
-              autoFocus="on"
-              className="d-flex btn default SearchButton"
-            ></button>
-          </form>
-        </div>
-      </header>
-      <main className="WeatherApp-main">
-        <PlaceInfo />
-      </main>
-      <aside className="WeatherApp-aside-1">
-        <WeatherTemperature />
-      </aside>
-      <aside className="WeatherApp-aside-2">
-        <WeatherProperties />
-      </aside>
-      <main className="WeatherApp-forecast">
-        <Forecast />
-      </main>
-      <footer className="WeatherApp-footer"></footer>
-    </div>
-  );
+export default function WeatherApp(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      city: response.data.name,
+      country: response.data.sys.country,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      feelsLike: response.data.main.feels_like,
+      lat: response.data.coord.lat,
+      lon: response.data.coord.lon,
+    });
+  }
+
+  function search() {
+    const apiEndpoint = `https://api.openweathermap.org/data/2.5/weather?`;
+    const apiKey = `fbea043a3c662cf4f3b5157ea45f2c8a`;
+    let apiUnits = `units=metric`;
+    let apiUrl = `${apiEndpoint}q=${city}&APPID=${apiKey}&${apiUnits}`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="WeatherApp">
+        <header className="WeatherApp-search">
+          <div className="d-flex justify-content-center Dictionary">
+            <form onSubmit={handleSubmit} className="d-flex form">
+              <input
+                type="search"
+                className="form-control"
+                autoComplete="off"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+              <button
+                type="submit"
+                value="Search"
+                autoFocus="on"
+                className="d-flex btn default SearchButton"
+              ></button>
+            </form>
+          </div>
+        </header>
+        <main className="WeatherApp-main">
+          <PlaceInfo data={weatherData} />
+        </main>
+        <aside className="WeatherApp-aside-1">
+          <WeatherTemperature />
+        </aside>
+        <aside className="WeatherApp-aside-2">
+          <WeatherProperties />
+        </aside>
+        <main className="WeatherApp-forecast">
+          <Forecast />
+        </main>
+        <footer className="WeatherApp-footer"></footer>
+      </div>
+    );
+  } else {
+    search();
+    return (
+      <div className="text-center Loader">
+        <Loader
+          type="Puff"
+          color="#00BFFF"
+          height={150}
+          width={150}
+          timeout={3000} //3 secs
+        />
+      </div>
+    );
+  }
 }
